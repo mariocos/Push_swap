8d9760f1d134c99ff945f9ccd736ef9c9a1feef0
mariocos <alpacasrcool7@gmail.com>
2025-03-08 23:50:49 +0000

    finished benchmark script

--- testers/bench.sh
+++ testers/bench.sh
@@ -15,7 +15,6 @@ for ((i=0 ; i<100 ; i++)); do
 	MOVES=$($PUSH_SWAP $ARG | wc -l)
 	AVERAGE=$(echo "$AVERAGE + $MOVES * 0.01" | bc)
 	RESULT=$($PUSH_SWAP $ARG | $CHECKER $ARG)
-	echo "$RESULT"
 
 	if [ "$RESULT" == "KO" ]; then
 		echo -e "\033[31mKO :(\033[0m"
@@ -27,6 +26,28 @@ for ((i=0 ; i<100 ; i++)); do
 done
 
 echo "done benchmarking for stacks of 100"
+echo "your benchmark is: $AVERAGE"
+
+echo ""
+echo "benchmark for stacks of 500"
+AVERAGE=0
+for ((i=0 ; i<10 ; i++)); do
+
+	ARG=$(shuf -i 1-1000 -n 500 | tr '\n' ' ')
+
+	MOVES=$($PUSH_SWAP $ARG | wc -l)
+	AVERAGE=$(echo "$AVERAGE + $MOVES * 0.1" | bc)
+	RESULT=$($PUSH_SWAP $ARG | $CHECKER $ARG)
+	if [ "$RESULT" == "KO" ]; then
+		echo -e "\033[31mKO :(\033[0m"
+		echo ""
+		echo $ARG
+		echo ""
+		break
+	fi
+done
+
+echo "done benchmarking for stacks of 500"
 echo "your benchmark is: $AVERAGE"
 
 rm -f Push_swap
