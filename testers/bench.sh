772632df486fa24981210ef577751d1f34862d83
mariocos <alpacasrcool7@gmail.com>
2025-03-08 22:24:58 +0000

    made the bench.sh work am now going to improve it

--- testers/bench.sh
+++ testers/bench.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+cp ../Push_swap .
+
 PUSH_SWAP=./Push_swap
-CHECKER=./testers/checker_linux
+CHECKER=./checker_linux
+AVERAGE=0
 
 echo "Going to bench mark your Push_swap :)"
 
 for ((i=0 ; i<100 ; i++)); do
 
-	ARG=$(shuf -i 1-1000 -n 100)
+	ARG=$(shuf -i 1-1000 -n 100 | tr '\n' ' ')
 
-	MOVES=$(PUSH_SWAP ARG | wc -l)
+	MOVES=$($PUSH_SWAP $ARG | wc -l)
 	AVERAGE=$(echo "$AVERAGE + $MOVES * 0.01" | bc)
 	RESULT=$($PUSH_SWAP $ARG | $CHECKER $ARG)
 	echo "$RESULT"
@@ -25,3 +28,5 @@ done
 
 echo "done benchmarking for stacks of 100"
 echo "your benchmark is: $AVERAGE"
+
+rm -f Push_swap
